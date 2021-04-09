55e1a70b6c39a8994f38d1ce8c93eebab881aa89
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-04-09 20:32:30 +0200

    completed

--- assets/main.js
+++ assets/main.js
@@ -3,14 +3,12 @@ Vue.config.devtools = true;
 var app = new Vue ({
     el: '#root',
     data: {
-      disks: [
-        {
-          poster: "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
-          title: "New Jersey",
-          author: "Bon Jovi",
-          genre: "Rock",
-          year: "1988"
-        }
-      ]
+      disks: []
     },
+    mounted() {
+      axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then((response) => {
+        this.disks = response.data.response;
+      });
+    }
   });
