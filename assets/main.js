2a6a59b46ebe531f50759cd00a9d27cdeec207ec
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-04-09 18:28:04 +0200

    dynamic objects with vue

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,16 @@
 Vue.config.devtools = true;
 
-var app = new Vue(
-  {
+var app = new Vue ({
     el: '#root',
     data: {
-
+      disks: [
+        {
+          poster: "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+          title: "New Jersey",
+          author: "Bon Jovi",
+          genre: "Rock",
+          year: "1988"
+        }
+      ]
     },
-  }
-);
+  });
